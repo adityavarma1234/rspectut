5f4e83d19899d5c7eb9d979b9754f71debdd3104
Aditya Varma <aditya.varma@zoomcar.com>
2015-07-21 12:36:24 +0530

    Add decapitate method

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,3 +1,9 @@
 class Zombie < ActiveRecord::Base
   validates :name, presence: true
+  has_one :weapon
+  
+  def decapitate
+    weapon.slice(self, :head)
+    self.status = "dead again"
+  end
 end
